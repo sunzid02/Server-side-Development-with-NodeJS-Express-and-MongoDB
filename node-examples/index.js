bbc119967787c66de4bda9785d64ac98d4ddef56
sunzid02 <sunzid02@gmail.com>
2020-07-08 03:06:45 +0600

    Node Modules: Callbacks and Error Handling

--- node-examples/index.js
+++ node-examples/index.js
@@ -4,18 +4,25 @@ function solveRect(l, b)
 {
     console.log("solving for rectangle l ="+ l + " and b = "+ b);
     
-    if ( l <=0 || b <= 0) 
-    {
-        console.log("rect dimension should be greater than 0");
-    }
-    else
-    {
-        console.log("The area of the rectangle is "+ rect.area(l, b) );        
-        console.log("The perimeter of the rectangle is "+ rect.perimeter(l, b) );        
-    }
+    rect(l, b, (err, rectangle) => {
+        
+        if (err) 
+        {
+            console.log("ERROR: ", err.message);
+        }
+        else 
+        {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }        
+    });
+    console.log("This statement after the call to rect()");
+
 }
 
 solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
+// solveRect(3, 5);
+// solveRect(0, 5);
 solveRect(-3, 5);
--- node-examples/rectangle.js
+++ node-examples/rectangle.js
@@ -1,2 +1,31 @@
-exports.perimeter = (x, y) => (2 * (x + y));
-exports.area = (x, y) => (x * y);
+
+// First - class functions: A	function can	be	treated	the
+// same	way as any	other	variable
+// x=length, y=bredth
+
+module.exports = (x, y, callback) => {
+    
+    if (x <= 0 || y <= 0) 
+    {
+        setTimeout(() => {
+            callback( new Error("Rectangle dimensions should be greater than zero: l = " + x + ", and b = " + y), null)
+        }, 2000);
+
+
+    }
+    else 
+    {
+        setTimeout( () =>{
+            callback(null, {
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+            })
+        }, 2000);
+    }
+}
+
+
+
+
+
+
