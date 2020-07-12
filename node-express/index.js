a886b6bd0bf6596794a566929f1cf4bd6ba2b749
sunzid02 <sunzid02@gmail.com>
2020-07-13 00:10:51 +0600

    Express Router

--- node-express/index.js
+++ node-express/index.js
@@ -15,49 +15,27 @@ app.use( bodyParser.json() );
 
 
 //////////////////// Routes starts /////////////////////////////////
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+const dishRouter = require('./routes/dishRouter');
 
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
+app.use('/dishes', dishRouter);
+// app.get('/dishes/:dishId', (req, res, next) => {
+//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+// });
 
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: '+ req.body.name + ' with details: ' + 
-    req.body.description);
-});
+// app.post('/dishes/:dishId', (req, res, next) => {
+//     res.statusCode = 403;
+//     res.end('POST operation not supported on /dishes/' + req.params.dishId);
+// });
 
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT not supported in /dishes');
-});
+// app.put('/dishes/:dishId', (req, res, next) => {
+//     res.write('Updating the dish: ' + req.params.dishId + '\n');
+//     res.end('Will update the dish: ' + req.body.name +
+//         ' with details: ' + req.body.description);
+// });
 
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-
-
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+// app.delete('/dishes/:dishId', (req, res, next) => {
+//     res.end('Deleting dish: ' + req.params.dishId);
+// });
 
 //////////////////// Routes ends /////////////////////////////////
 
