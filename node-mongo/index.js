a6d8e2785e5729eadec0d3dfdaff53a575fa4974
sunzid02 <sunzid02@gmail.com>
2020-07-15 16:46:21 +0600

    Callback Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -5,36 +5,40 @@ const dboper = require('./operations');
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url).then((client) => {
 
-    assert.equal(err, null);
-
-    console.log('Connected correctly to the server');
-
+    console.log('Connected correctly to server');
     const db = client.db(dbname);
 
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes", (result) => {
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes")
+        .then((result) => {
             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Documents:\n", docs);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+            return dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes");
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
+        })
+        .then((result) => {
+            console.log("Updated Document:\n", result.result);
 
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("Found Updated Documents:\n", docs);
 
-                                client.close();
-                            });
-                        });
-                    });
-            });
-        });
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("Dropped Collection: ", result);
 
-});
+            return client.close();
+        })
+        .catch((err) => console.log(err));
+
+    })
+    .catch((err) => console.log(err));
--- node-mongo/operations.js
+++ node-mongo/operations.js
@@ -4,14 +4,7 @@ exports.insertDocument = (db, document, collection, callback) => {
     
     const coll = db.collection(collection);
 
-    coll.insert( document, (err, result) => {
-        assert.equal(err, null);
-
-        console.log("Inserted " + result.result.n + " documents into the collection" );
-
-        callback(result);
-        
-    });
+    return coll.insert( document);
 };
 
 
@@ -19,12 +12,7 @@ exports.findDocuments = (db, collection, callback) => {
 
     const coll = db.collection(collection);
 
-    coll.find({}).toArray( (err, docs) => {
-
-        assert.equal(err, null);
-        callback(docs);
-
-    });
+    return coll.find({}).toArray();
 
 };
 
@@ -33,13 +21,7 @@ exports.removeDocument = (db, document, collection, callback) => {
 
     const coll = db.collection(collection);
 
-    coll.deleteOne( document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-
-        callback(result);
-        
-    });
+    return coll.deleteOne( document);
 
 };
 
@@ -48,11 +30,5 @@ exports.updateDocument = (db, document, update, collection, callback) => {
 
     const coll = db.collection(collection);
 
-    coll.updateOne(document, { $set: update}, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document ", update);
-        
-        callback(result);
-
-    });
+    return coll.updateOne(document, { $set: update}, null );
 };
